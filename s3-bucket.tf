c2d77d07d80331f52f67661f0a7a458a3525c38f
yrheo <root@p-odx7ypwxmbox-workstation.c.instruqt-prod.internal>
2021-01-26 06:16:39 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,6 +1,6 @@
 module "s3-bucket" {
   source  = "app.terraform.io/unipoint01/s3-bucket/aws"
-  version = "1.15.0"
+  version = "1.17.0"
   # insert required variables here
   bucket_prefix = "yrheo"
 }
